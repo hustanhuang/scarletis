e8c8ae93471635f55001a288d7ff5a1527108f89
Andrew Huang <anhuang@hustunique.com>
2015-12-02 20:04:24 +0800

    update server.c
    improve logging method

--- src/server.c
+++ src/server.c
@@ -5,59 +5,67 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+#include <time.h>
+
 #define SCAR_PORT 6379
 
 const char *welcome =
 "Scarletis\n"
 "Port: %d\n";
 
+time_t g_ticks;
+
+void s_log(const char *s) {
+    g_ticks = time(NULL);
+    fprintf(stdout, "%.24s * %s\n", ctime(&g_ticks), s);
+}
+
+void s_errlog(const char *s) {
+    g_ticks = time(NULL);
+    fprintf(stderr, "%.24s # ", ctime(&g_ticks));
+    perror(s);
+    exit(EXIT_FAILURE);
+}
+
 int main()
 {
     fprintf(stdout, welcome, SCAR_PORT);
 
     int listen_fd;
-    if ( (listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("retrieve socket");
-        exit(EXIT_FAILURE);
-    }
+    if ( (listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        s_errlog("retrieve socket");
 
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(SCAR_PORT);
 
-    if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("bind address");
-        exit(EXIT_FAILURE);
-    }
+    if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        s_errlog("bind address");
 
-    if (listen(listen_fd, 16) < 0) {
-        perror("listening");
-        exit(EXIT_FAILURE);
-    }
+    if (listen(listen_fd, 16) < 0)
+        s_errlog("listening");
 
     int conn_fd;
     char buff[1025];
     for ( ; ; ) {
-        if ( (conn_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL)) < 0) {
-            perror("accepting connections");
-            exit(EXIT_FAILURE);
-        }
 
-        for (int i = 0; i != 10; ++i) {
-            snprintf(buff, sizeof(buff), "\b\b\b\b\b\bandyroiiid");
-            if (write(conn_fd, buff, strlen(buff)) < 0) {
-                perror("write into socket");
-                exit(EXIT_FAILURE);
-            }
-            sleep(1);
-        }
+        s_log("Waiting for connections");
 
-        if (close(conn_fd) < 0) {
-            perror("closing connection");
-            exit(EXIT_FAILURE);
-        }
+        if ( (conn_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL)) < 0)
+            s_errlog("accepting connections");
+
+        s_log("Write into socket");
+
+        snprintf(buff, sizeof(buff), "hello,world\r\n");
+        if (write(conn_fd, buff, strlen(buff)) < 0)
+            s_errlog("write into socket");
+
+        if (close(conn_fd) < 0)
+            s_errlog("closing connection");
+
+        s_log("Session complete");
     }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }

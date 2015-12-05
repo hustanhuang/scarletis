a0ed8e958d80b1fd51bb83ce30cc4291f2819875
Andrew Huang <anhuang@hustunique.com>
2015-12-05 15:43:54 +0800

    update cmds
    add commands

--- src/cmds.c
+++ src/cmds.c
@@ -24,16 +24,592 @@ CMD_SIGN(hello) {
     return 0;
 }
 
-CMD_SIGN(bye) {
+CMD_SIGN(append) {
     char buff[BUFF_LEN];
-    strcpy(buff, "Bye");
+    strcpy(buff, "params:");
 
     list_t *pos = NULL;
     list_for_each(pos, paras) {
-        strcat(buff, ", ");
+        strcat(buff, " ");
         strcat(buff, param_value(pos));
     }
-    strcat(buff, ".\r\n");
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(bitcount) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(brpop) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(brpoplpush) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(decr) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(decrby) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(del) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(exists) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(get) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(getbit) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(getrange) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(incr) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(incrby) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(keys) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lindex) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(linsert) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(llen) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lpop) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lpush) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lpushx) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lrange) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lrem) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(lset) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(ltrim) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(mget) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(msetnx) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(randomkey) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(rename) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(rpop) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(rpoplpush) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(rpush) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(rpushx) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(set) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(setbit) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(setrange) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(strlen) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
+
+    if (send(conn_fd, buff, strlen(buff), 0) < 0)
+        s_err("send");
+    return 0;
+}
+
+CMD_SIGN(type) {
+    char buff[BUFF_LEN];
+    strcpy(buff, "params:");
+
+    list_t *pos = NULL;
+    list_for_each(pos, paras) {
+        strcat(buff, " ");
+        strcat(buff, param_value(pos));
+    }
+    strcat(buff, "\r\n");
 
     if (send(conn_fd, buff, strlen(buff), 0) < 0)
         s_err("send");
--- src/cmds.h
+++ src/cmds.h
@@ -6,6 +6,42 @@
 #define CMD_SIGN(name) int cmd_##name(int conn_fd, list_t *paras)
 
 CMD_SIGN(hello);
-CMD_SIGN(bye);
+CMD_SIGN(append);
+CMD_SIGN(bitcount);
+CMD_SIGN(brpop);
+CMD_SIGN(brpoplpush);
+CMD_SIGN(decr);
+CMD_SIGN(decrby);
+CMD_SIGN(del);
+CMD_SIGN(exists);
+CMD_SIGN(get);
+CMD_SIGN(getbit);
+CMD_SIGN(getrange);
+CMD_SIGN(incr);
+CMD_SIGN(incrby);
+CMD_SIGN(keys);
+CMD_SIGN(lindex);
+CMD_SIGN(linsert);
+CMD_SIGN(llen);
+CMD_SIGN(lpop);
+CMD_SIGN(lpush);
+CMD_SIGN(lpushx);
+CMD_SIGN(lrange);
+CMD_SIGN(lrem);
+CMD_SIGN(lset);
+CMD_SIGN(ltrim);
+CMD_SIGN(mget);
+CMD_SIGN(msetnx);
+CMD_SIGN(randomkey);
+CMD_SIGN(rename);
+CMD_SIGN(rpop);
+CMD_SIGN(rpoplpush);
+CMD_SIGN(rpush);
+CMD_SIGN(rpushx);
+CMD_SIGN(set);
+CMD_SIGN(setbit);
+CMD_SIGN(setrange);
+CMD_SIGN(strlen);
+CMD_SIGN(type);
 
 #endif
